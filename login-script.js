430b1d0893a2181bc0dd4ce2c133fbc1e361c59f
Lucas <lucasrarueira013@gmail.com>
2024-11-01 09:28:18 -0300

    natsuflix

--- login-script.js
+++ login-script.js
@@ -1,17 +1,18 @@
 // Defina as credenciais diretamente no código
-const validUsername = "natsumi";
-const validPassword = "1234";
+const validUsername = "natsumi"; // Nome de usuário válido
+const validPassword = "1234";     // Senha válida
 
+// Adiciona o evento de submit ao formulário
 document.getElementById('loginForm').addEventListener('submit', function (e) {
-    e.preventDefault();
+    e.preventDefault(); // Evita o envio padrão do formulário
 
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
+    const username = document.getElementById('username').value; // Obtém o nome de usuário
+    const password = document.getElementById('password').value; // Obtém a senha
 
     // Verifica se as credenciais correspondem
     if (username === validUsername && password === validPassword) {
         alert("Login bem-sucedido!");
-        window.location.href = "home.html";
+        window.location.href = "home.html"; // Redireciona para a página home
     } else {
         alert("Usuário ou senha incorretos.");
     }
@@ -20,7 +21,7 @@ document.getElementById('loginForm').addEventListener('submit', function (e) {
 // Lista de URLs das imagens de fundo
 const backgroundImages = [
     'https://images.alphacoders.com/101/thumb-1920-1010799.png',
-    'https://i.pinimg.com/originals/29/e7/a9/29e7a9c0aac67dafb758cf35b20c8bef.jpg',
+    'https://wallpapercg.com/download/lelouch-lamperouge-5782x3816-11234.jpg',
     'https://wallpapers.com/images/hd/blue-themed-yuyu-hakusho-lb88p89u45oz29ks.jpg',
     'https://images8.alphacoders.com/136/thumb-1920-1369308.png'
 ];
