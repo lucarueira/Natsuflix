81df8191f162379e239fe19c076863c1340bfeda
lucarueira <lucasrarueira017@gmail.com>
2024-12-23 04:14:22 -0300

    natsuflix

--- animes/Steins-Gate/steins-gate.js
+++ animes/Steins-Gate/steins-gate.js
@@ -2,17 +2,29 @@
 const episodios = [
     { title: "Episódio 1 ", link: "https://drive.google.com/file/d/1ToT63ECwowG2BqRGdnzweoQg6XoJh0cR/preview", img: "https://m.media-amazon.com/images/S/pv-target-images/bd78eabf47823f52eb98d2a89fca34cdb9dacd39cacc9465f2f811e05cc8ac45.jpg" },
     { title: "Episódio 2 ", link: "https://drive.google.com/file/d/14faPJCPq5YdOAHflCzkzipZ-teN5RAtC/preview", img: "https://m.media-amazon.com/images/S/pv-target-images/bd78eabf47823f52eb98d2a89fca34cdb9dacd39cacc9465f2f811e05cc8ac45.jpg" },
-    { title: "Episódio 3 ", link: "https://drive.google.com/file/d/1hpxjRNeBzzgd9e5w0WZBKpFvGxE1qJb4/preview", img: "https://m.media-amazon.com/images/S/pv-target-images/bd78eabf47823f52eb98d2a89fca34cdb9dacd39cacc9465f2f811e05cc8ac45.jpg" },
-    { title: "Episódio 4 ", link: "https://drive.google.com/file/d/16GUSVU5J7VHr97J6_JkPsusK9q5icubl/preview", img: "https://m.media-amazon.com/images/S/pv-target-images/bd78eabf47823f52eb98d2a89fca34cdb9dacd39cacc9465f2f811e05cc8ac45.jpg" },
-    { title: "Episódio 5 ", link: "https://drive.google.com/file/d/1a0FpEJIiQZ6wNwAOVrJXoyO0cFT4eJnU/preview", img: "https://m.media-amazon.com/images/S/pv-target-images/bd78eabf47823f52eb98d2a89fca34cdb9dacd39cacc9465f2f811e05cc8ac45.jpg" },
-    { title: "Episódio 6 ", link: "https://drive.google.com/file/d/16tBLRcnq-HB9VM54t-ZMdpFrdPrwjnD6/preview", img: "https://m.media-amazon.com/images/S/pv-target-images/bd78eabf47823f52eb98d2a89fca34cdb9dacd39cacc9465f2f811e05cc8ac45.jpg" },
-    { title: "Episódio 7 ", link: "https://drive.google.com/file/d/1MDWIS8nAMvZ2Rqo0c_TEvexvf1OYSwpq/preview", img: "https://m.media-amazon.com/images/S/pv-target-images/bd78eabf47823f52eb98d2a89fca34cdb9dacd39cacc9465f2f811e05cc8ac45.jpg" },
-    { title: "Episódio 8 ", link: "https://drive.google.com/file/d/13-lGSCyx3x2v07wcvCR9IgCPjOacYqEr/preview", img: "https://m.media-amazon.com/images/S/pv-target-images/bd78eabf47823f52eb98d2a89fca34cdb9dacd39cacc9465f2f811e05cc8ac45.jpg" },
-    { title: "Episódio 9 ", link: "https://drive.google.com/file/d/1CPawpmpEBXjABWGm5yZtFiUGi0WvNuVg/preview", img: "https://m.media-amazon.com/images/S/pv-target-images/bd78eabf47823f52eb98d2a89fca34cdb9dacd39cacc9465f2f811e05cc8ac45.jpg" },
-    { title: "Episódio 10 ", link: "https://drive.google.com/file/d/12qiCV1P4JkELeBtxRyfc21JTrQ_63S5P/preview", img: "https://m.media-amazon.com/images/S/pv-target-images/bd78eabf47823f52eb98d2a89fca34cdb9dacd39cacc9465f2f811e05cc8ac45.jpg" },
- 
- 
- 
+    { title: "Episódio 3 ", link: "https://drive.google.com/file/d/1Emo_P647RjjYM3Yg_4BR_1GkLnUHXAH6/preview", img: "https://m.media-amazon.com/images/S/pv-target-images/bd78eabf47823f52eb98d2a89fca34cdb9dacd39cacc9465f2f811e05cc8ac45.jpg" },
+    { title: "Episódio 4 ", link: "https://drive.google.com/file/d/1S_7z-otH-GNJwEjZo2-FJfQEwA5gWNtf/preview", img: "https://m.media-amazon.com/images/S/pv-target-images/bd78eabf47823f52eb98d2a89fca34cdb9dacd39cacc9465f2f811e05cc8ac45.jpg" },
+    { title: "Episódio 5 ", link: "https://drive.google.com/file/d/1pXwOMdVGp5GZpAAI-xR5ArvDXcycJTuv/preview", img: "https://m.media-amazon.com/images/S/pv-target-images/bd78eabf47823f52eb98d2a89fca34cdb9dacd39cacc9465f2f811e05cc8ac45.jpg" },
+    { title: "Episódio 6 ", link: "https://drive.google.com/file/d/1RxZ4_nPN7-r8Q9gwj2kxP5CamQNvq7RR/preview", img: "https://m.media-amazon.com/images/S/pv-target-images/bd78eabf47823f52eb98d2a89fca34cdb9dacd39cacc9465f2f811e05cc8ac45.jpg" },
+    { title: "Episódio 7 ", link: "https://drive.google.com/file/d/1O7v7L0Px9FtDbC6A3LmL_0Y0Q_WO0PoI/preview", img: "https://m.media-amazon.com/images/S/pv-target-images/bd78eabf47823f52eb98d2a89fca34cdb9dacd39cacc9465f2f811e05cc8ac45.jpg" },
+    { title: "Episódio 8 ", link: "https://drive.google.com/file/d/1if0cNWHuwSsMS081NycpQQb4OijlVGGP/preview", img: "https://m.media-amazon.com/images/S/pv-target-images/bd78eabf47823f52eb98d2a89fca34cdb9dacd39cacc9465f2f811e05cc8ac45.jpg" },
+    { title: "Episódio 9 ", link: "https://drive.google.com/file/d/1npZufR8NSVLfwxdkzzDa-1U7X8MxoFn3/preview", img: "https://m.media-amazon.com/images/S/pv-target-images/bd78eabf47823f52eb98d2a89fca34cdb9dacd39cacc9465f2f811e05cc8ac45.jpg" },
+    { title: "Episódio 10 ", link: "https://drive.google.com/file/d/1hb_6zcXm3HJmW4CA8gMSmL5qe79hiPyB/preview", img: "https://m.media-amazon.com/images/S/pv-target-images/bd78eabf47823f52eb98d2a89fca34cdb9dacd39cacc9465f2f811e05cc8ac45.jpg" },
+    { title: "Episódio 11 ", link: "https://drive.google.com/file/d/1vJA5MsH-FWjWZZdplnnQ74SUo6jhb6Qk/preview", img: "https://m.media-amazon.com/images/S/pv-target-images/bd78eabf47823f52eb98d2a89fca34cdb9dacd39cacc9465f2f811e05cc8ac45.jpg" },
+    { title: "Episódio 12 ", link: "https://drive.google.com/file/d/18QB6ic-XDOyAR-_3ed7UwEt-u3vPETbS/preview", img: "https://m.media-amazon.com/images/S/pv-target-images/bd78eabf47823f52eb98d2a89fca34cdb9dacd39cacc9465f2f811e05cc8ac45.jpg" },
+    { title: "Episódio 13 ", link: "https://drive.google.com/file/d/1_OWgqu5R3orLrcMOFFdC8q8xUL-qQRh2/preview", img: "https://m.media-amazon.com/images/S/pv-target-images/bd78eabf47823f52eb98d2a89fca34cdb9dacd39cacc9465f2f811e05cc8ac45.jpg" },
+    { title: "Episódio 14 ", link: "https://drive.google.com/file/d/1ALHlPS8rWFKJAm6OpU6-768nwiFwgPmq/preview", img: "https://m.media-amazon.com/images/S/pv-target-images/bd78eabf47823f52eb98d2a89fca34cdb9dacd39cacc9465f2f811e05cc8ac45.jpg" },
+    { title: "Episódio 15 ", link: "https://drive.google.com/file/d/1ly2_GYCPtdQuuQlwNms8vOYqEaSDjkRg/preview", img: "https://m.media-amazon.com/images/S/pv-target-images/bd78eabf47823f52eb98d2a89fca34cdb9dacd39cacc9465f2f811e05cc8ac45.jpg" },
+    { title: "Episódio 16 ", link: "https://drive.google.com/file/d/1Y18W0LyjSA01wN9rzRBsZnGU7ECGpR_U/preview", img: "https://m.media-amazon.com/images/S/pv-target-images/bd78eabf47823f52eb98d2a89fca34cdb9dacd39cacc9465f2f811e05cc8ac45.jpg" },
+    { title: "Episódio 17 ", link: "https://drive.google.com/file/d/1bwGngTEbk_3JQDeQPwM5qgEEf0AAT6Fp/preview", img: "https://m.media-amazon.com/images/S/pv-target-images/bd78eabf47823f52eb98d2a89fca34cdb9dacd39cacc9465f2f811e05cc8ac45.jpg" },
+    { title: "Episódio 18 ", link: "https://drive.google.com/file/d/12QrXpjB53G-GyVQ8OkTqcAZWr3-ahUKu/preview", img: "https://m.media-amazon.com/images/S/pv-target-images/bd78eabf47823f52eb98d2a89fca34cdb9dacd39cacc9465f2f811e05cc8ac45.jpg" },
+    { title: "Episódio 19", link: "https://drive.google.com/file/d/11yoeRohrtZTYpkdW4d8lOWvDkaJtd3vH/preview", img: "https://m.media-amazon.com/images/S/pv-target-images/bd78eabf47823f52eb98d2a89fca34cdb9dacd39cacc9465f2f811e05cc8ac45.jpg" },
+    { title: "Episódio 20 ", link: "https://drive.google.com/file/d/1M8z66-uPi_NUg20jwco_6ios_NS8d-fT/preview", img: "https://m.media-amazon.com/images/S/pv-target-images/bd78eabf47823f52eb98d2a89fca34cdb9dacd39cacc9465f2f811e05cc8ac45.jpg" },
+    { title: "Episódio 21 ", link: "https://drive.google.com/file/d/1anOBrRsesauguD4fE9FZYhOP53TtZKiU/preview", img: "https://m.media-amazon.com/images/S/pv-target-images/bd78eabf47823f52eb98d2a89fca34cdb9dacd39cacc9465f2f811e05cc8ac45.jpg" },
+    { title: "Episódio 22 ", link: "https://drive.google.com/file/d/1WXoYB8UWICjVphxrsC1mh9Z3jVECNVBv/preview", img: "https://m.media-amazon.com/images/S/pv-target-images/bd78eabf47823f52eb98d2a89fca34cdb9dacd39cacc9465f2f811e05cc8ac45.jpg" },
+    { title: "Episódio 23 ", link: "https://drive.google.com/file/d/19xNuz3n1d-hXZ-fwE16vvdA-xYmyFYMM/preview", img: "https://m.media-amazon.com/images/S/pv-target-images/bd78eabf47823f52eb98d2a89fca34cdb9dacd39cacc9465f2f811e05cc8ac45.jpg" },
+    { title: "Episódio 24", link: "https://drive.google.com/file/d/1biqnv8vr-BzNFTpy9lQ8xnDFZj9azf3w/preview", img: "https://m.media-amazon.com/images/S/pv-target-images/bd78eabf47823f52eb98d2a89fca34cdb9dacd39cacc9465f2f811e05cc8ac45.jpg" },
+    { title: "Episódio 25 ", link: "https://drive.google.com/file/d/1xIutRNcdmIXtVUYINhElLyRz45sVcRN2/preview", img: "https://m.media-amazon.com/images/S/pv-target-images/bd78eabf47823f52eb98d2a89fca34cdb9dacd39cacc9465f2f811e05cc8ac45.jpg" },
  
    
 ];
--- animes/katekyo-hitman-reborn/katekyo-hitman-reborn-script.js
+++ animes/katekyo-hitman-reborn/katekyo-hitman-reborn-script.js
@@ -10,8 +10,27 @@ const episodios = [
     { title: "Episódio 8", link: "https://drive.google.com/file/d/129qR9dGgoW3nZ31dgiRBVGs_ESGW_hce/preview", img: "https://noticias.animeonegai.com/wp-content/uploads/2022/09/Katekyo-Hitman-Reborn-anime-min-2.jpg" },
     { title: "Episódio 9", link: "https://drive.google.com/file/d/15gFyiyKJgev3xlQ0_FPfuKnWjKRBtBTJ/preview", img: "https://noticias.animeonegai.com/wp-content/uploads/2022/09/Katekyo-Hitman-Reborn-anime-min-2.jpg" },
     { title: "Episódio 10", link: "https://drive.google.com/file/d/1QWJnKttqVC5-pREnxkmcQaZR4MtTBJtC/preview", img: "https://noticias.animeonegai.com/wp-content/uploads/2022/09/Katekyo-Hitman-Reborn-anime-min-2.jpg" },
+    { title: "Episódio 11", link: "https://drive.google.com/file/d/1Tq3Ic3ET0PK_1dWFNK8qFU4su4XXEFWZ/preview", img: "https://noticias.animeonegai.com/wp-content/uploads/2022/09/Katekyo-Hitman-Reborn-anime-min-2.jpg" },
+    { title: "Episódio 12", link: "https://drive.google.com/file/d/1ACKq9JIBHAvUZSq8eu2Kf4o9UWOlaTQy/preview", img: "https://noticias.animeonegai.com/wp-content/uploads/2022/09/Katekyo-Hitman-Reborn-anime-min-2.jpg" },
+    { title: "Episódio 13", link: "https://drive.google.com/file/d/1ZtzHC5rgrpi7EKQuAHSdMCepuUuSpq8x/preview", img: "https://noticias.animeonegai.com/wp-content/uploads/2022/09/Katekyo-Hitman-Reborn-anime-min-2.jpg" },
+    { title: "Episódio 14", link: "https://drive.google.com/file/d/1XksZjl4EZ7xd7oN0gTqQigfesdFGycgR/preview", img: "https://noticias.animeonegai.com/wp-content/uploads/2022/09/Katekyo-Hitman-Reborn-anime-min-2.jpg" },
+    { title: "Episódio 15", link: "https://drive.google.com/file/d/17dEyxZB-iAAqLAY4KyYwyrWGCi7yWFmI/preview", img: "https://noticias.animeonegai.com/wp-content/uploads/2022/09/Katekyo-Hitman-Reborn-anime-min-2.jpg" },
+    { title: "Episódio 16", link: "https://drive.google.com/file/d/1xDCGPhpavhGapW3huYP4N8hUNH1xINsu/preview", img: "https://noticias.animeonegai.com/wp-content/uploads/2022/09/Katekyo-Hitman-Reborn-anime-min-2.jpg" },
+    { title: "Episódio 17", link: "https://drive.google.com/file/d/1R84JkRc2xeXBFvBzSLLrCLSl3ajpgvoP/preview", img: "https://noticias.animeonegai.com/wp-content/uploads/2022/09/Katekyo-Hitman-Reborn-anime-min-2.jpg" },
+    { title: "Episódio 18", link: "https://drive.google.com/file/d/1rAb-koF-p3GsPh7zbrxeeJsSxIGDOU6y/preview", img: "https://noticias.animeonegai.com/wp-content/uploads/2022/09/Katekyo-Hitman-Reborn-anime-min-2.jpg" },
+    { title: "Episódio 19", link: "https://drive.google.com/file/d/1j42F6JQPc1h6HYxdMsfJsJq4v52XiZHb/preview", img: "https://noticias.animeonegai.com/wp-content/uploads/2022/09/Katekyo-Hitman-Reborn-anime-min-2.jpg" },
+    { title: "Episódio 20", link: "https://drive.google.com/file/d/1FE_23Eda-U0hXzznIrtLv_MUdN5CACOM/preview", img: "https://noticias.animeonegai.com/wp-content/uploads/2022/09/Katekyo-Hitman-Reborn-anime-min-2.jpg" },
+    { title: "Episódio 21", link: "https://drive.google.com/file/d/1s20HNKE77tco9gBxz2pu_UyugmxkkSe4/preview", img: "https://noticias.animeonegai.com/wp-content/uploads/2022/09/Katekyo-Hitman-Reborn-anime-min-2.jpg" },
+    { title: "Episódio 22", link: "https://drive.google.com/file/d/1n-oCSgk5cOKBAVi2bPpLhHyTjIai60oz/preview", img: "https://noticias.animeonegai.com/wp-content/uploads/2022/09/Katekyo-Hitman-Reborn-anime-min-2.jpg" },
+    { title: "Episódio 23", link: "https://drive.google.com/file/d/1Pf55E2CUhoRiiGqyWvs4l162Cud7-bg1/preview", img: "https://noticias.animeonegai.com/wp-content/uploads/2022/09/Katekyo-Hitman-Reborn-anime-min-2.jpg" },
+    { title: "Episódio 24", link: "https://drive.google.com/file/d/1hJ1heUqiItqyd5s2inBvNokm3UlC1Iof/preview", img: "https://noticias.animeonegai.com/wp-content/uploads/2022/09/Katekyo-Hitman-Reborn-anime-min-2.jpg" },
+    { title: "Episódio 25", link: "https://drive.google.com/file/d/1VXgn2eZMuEKzwPv4AZ8rMIpFdQFW5WLN/preview", img: "https://noticias.animeonegai.com/wp-content/uploads/2022/09/Katekyo-Hitman-Reborn-anime-min-2.jpg" },
+    { title: "Episódio 26", link: "https://drive.google.com/file/d/1Dv_uxU07vGPkJ-miIw6huc0PoaLLwDWp/preview", img: "https://noticias.animeonegai.com/wp-content/uploads/2022/09/Katekyo-Hitman-Reborn-anime-min-2.jpg" },
+    { title: "Episódio 27", link: "https://drive.google.com/file/d/1c6VaUZLLOlT3vceq5IM2PTwDGCQhf9C5/preview", img: "https://noticias.animeonegai.com/wp-content/uploads/2022/09/Katekyo-Hitman-Reborn-anime-min-2.jpg" },
+    { title: "Episódio 28", link: "https://drive.google.com/file/d/1IV37U-eaSelOqXyqZ6RDN4UQSnPZEqKY/preview", img: "https://noticias.animeonegai.com/wp-content/uploads/2022/09/Katekyo-Hitman-Reborn-anime-min-2.jpg" },
+    { title: "Episódio 29", link: "https://drive.google.com/file/d/1FFHhUeuikpVHA18Fi0hmronDUuSA3otl/preview", img: "https://noticias.animeonegai.com/wp-content/uploads/2022/09/Katekyo-Hitman-Reborn-anime-min-2.jpg" },
+    { title: "Episódio 30", link: "https://drive.google.com/file/d/1rGkCH_d8sX3x7cXS0RpB16ldrJto7ua7/preview", img: "https://noticias.animeonegai.com/wp-content/uploads/2022/09/Katekyo-Hitman-Reborn-anime-min-2.jpg" },
     
-   
 ];
 
 // Função para gerar os episódios
